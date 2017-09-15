9ea06e3c75f341e1416c825c3bbd7eeb4ba8c702
nikki2lol <nikki2lol@mail.ru>
2017-09-15 13:57:35 +0300

    Окончательный вариант с .min

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var link = document.querySelector(".search");
 var popup = document.querySelector(".appointment-form");
 
-  popup.classList.add("appointment-form-closed");
+popup.classList.add("appointment-form-closed");
 link.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.toggle("appointment-form-closed");
