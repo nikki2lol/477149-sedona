6efb07d280eb45f17d3187a327a418b14751e217
nikki2lol <nikki2lol@mail.ru>
2017-09-15 13:37:11 +0300

    Ещё поправки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var link = document.querySelector(".search");
 var popup = document.querySelector(".appointment-form");
 
+  popup.classList.add("appointment-form-closed");
 link.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.toggle("appointment-form-closed");
